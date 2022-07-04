fa04460728cc374b55fe8300e00d597b123b37a1
nicolas335 <nicolasemanuel335@gmail.com>
2022-07-04 14:12:14 -0300

    organizo css, falta organizacion y modificaciones

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require ('express')
-const app = express()
+const express = require ('express');
+const app = express();/*  funcion de alto nivel */
 const path = require("path");
 const port = 3000;
 
